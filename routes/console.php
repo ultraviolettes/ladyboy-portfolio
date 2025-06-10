87e054fb2de08e6675832a6ccc341c25f29102b9
Baptiste <baptiste@ultraviolettes.fr>
2025-06-10 18:41:23 +0200

    Adds command to regenerate WebP images
    Adds an Artisan command to regenerate all images in WebP format, including responsive images.
    
    This allows developers to easily refresh all WebP images within the application, ensuring that they are up-to-date.
    
    Also, adds `storage/media-library/` to the .gitignore file.

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,15 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('regenerate-webp', function () {
+    $this->info('Starting regeneration of all images in WebP format...');
+
+    // Call the Spatie MediaLibrary regenerate command with the appropriate options
+    // We're using --with-responsive-images to ensure all responsive variants are regenerated
+    $this->call('media-library:regenerate', [
+        '--with-responsive-images' => true,
+    ]);
+
+    $this->info('All images have been regenerated in WebP format!');
+})->purpose('Regenerate all images in WebP format');
